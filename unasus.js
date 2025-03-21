27f681eb95f8e2c0bb551aa5b08e7574f34ceb78
Yasmin Serejo <yasminserejo@gmail.com>
2025-03-21 12:05:04 -0300

    Limpeza Css e Comentario Js

--- unasus.js
+++ unasus.js
@@ -1,18 +1,14 @@
 // Adiciona o evento de clique nos botões de controle
 document.querySelectorAll('.carousel-btn').forEach((button, index) => {
   button.addEventListener('click', () => {
-    // Remove a classe 'active' de todos os botões
     document.querySelectorAll('.carousel-btn').forEach(btn => btn.classList.remove('active'));
-    
-    // Adiciona a classe 'active' no botão clicado
     button.classList.add('active');
   });
 });
 
+// Passa seta do lado para o outro
 function moveCarousel(direction) {
   if (direction === 'prev') {
-    // Lógica para mover o carrossel para o item anterior
   } else if (direction === 'next') {
-    // Lógica para mover o carrossel para o próximo item
   }
 }
